395a4debba2dae07bfc8b488caf72dabd94cfb92
DemonixOmega <51BrRisack@gmail.com>
2017-02-23 19:36:58 -0500

    Update Script1.py

--- Script1.py
+++ Script1.py
@@ -6,6 +6,9 @@ if(a = 15);
 b = 0
 
 import Script2.py
+from Script2.py import d
+from Script2.py import e
+from Script2.py import f
 # Attempt to import Script2's variables for testing
 if(c = 15);
 {
